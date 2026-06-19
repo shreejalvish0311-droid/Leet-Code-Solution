b27cb8cae2e14dee32ccea2f9b7911039c88b15f
shreejalvish0311-droid <shreejalvish0311@gmail.com>
2026-06-19 13:44:31 +0530

    Time: 0 ms (100%), Space: 20.4 MB (89.22%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        j = 0
+
+        # Move all non-zero elements to the front
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
